3c24ee6c0f8e3ec44780cac3a1b50cf3782f93ea
Riqzzz <tariqaziz2801@gmail.com>
2025-01-24 22:58:13 +0700

    function String

--- string.go
+++ string.go
@@ -6,5 +6,10 @@ func main() {
 	fmt.Println("Mr.")
 	fmt.Println("Towick")
 	fmt.Println("Mr.Towick")
+	// -----------------------------
 
+	// Func String
+	fmt.Println(len("Mr."))     //panjang/jumlah character pada string
+	fmt.Println("Towick"[5])    //mengambil/menampilkan character pada string di array tertentu
+	fmt.Println("Mr.Towick"[1]) //mengambil/menampilkan character pada string di array tertentu
 }
