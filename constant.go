bcd6df7f6f9effba1decd7c1ac4b087d4c5f3602
Riqzzz <tariqaziz2801@gmail.com>
2025-01-28 19:31:38 +0700

    Constant

---
+++ constant.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firstname = "Towick"
+	const lastname = "Riqzzz"
+	//const=variable yg static value tidak bisa diubah, var dinamis bisa diubah
+	//var harus dipakai, const tidak perlu (tidak eror)
+
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+}
