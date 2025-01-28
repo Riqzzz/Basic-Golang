c19228cb8b5fb0685f2af55f408c2f761bbfc41a
Riqzzz <tariqaziz2801@gmail.com>
2025-01-28 20:04:15 +0700

    Konveri Tipe Data 2

---
+++ conversion2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		name    = "Towick Riqzzz"
+		e       = name[3]
+		eString = string(e)
+	)
+
+	fmt.Println(name)
+	fmt.Println(e)
+	fmt.Println(eString)
+
+}
