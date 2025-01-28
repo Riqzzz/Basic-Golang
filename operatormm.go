6e7fcc2ce1d6877cc1feee3c31fc43f6d3720d94
Riqzzz <tariqaziz2801@gmail.com>
2025-01-28 21:31:47 +0700

    Unary Operator

--- operatormm.go
+++ operatormm.go
@@ -9,15 +9,27 @@ func main() {
 		d = 5
 		e = 8
 		c = b + a - e*d
+		i = 10
+		j = 5
 	)
 
 	fmt.Println("Nilai C adalah :", c)
+
 	fmt.Println("-------------------Augmented Assigment------------------")
-
-	var i = 10
 	i += 10
 	fmt.Println("Nilai i Sekarang = ", i)
 
 	i += 10
 	fmt.Println("Nilai i Sekarang = ", i)
+
+	fmt.Println("-------------------Unary Operator------------------")
+	j++
+	fmt.Println("Nilai J adalah ", j)
+	j++
+	fmt.Println("Nilai J++ Sekarang ", j)
+
+	j--
+	fmt.Println("Nilai J-- Sekarang ", j)
+	j--
+	fmt.Println("Nilai J-- Sekarang ", j)
 }
