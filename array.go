b57d88102b4e8732c6328703875622deb74f4806
Riqzzz <tariqaziz2801@gmail.com>
2025-02-19 23:16:16 +0700

    Function Array

--- array.go
+++ array.go
@@ -8,10 +8,14 @@ func main() {
 	names[0] = "Mr"
 	names[1] = "Towick"
 	names[2] = "Aizz"
-
 	fmt.Println(names[0], names[1], names[2])
 
 	var nilai = [3]int{90, 80, 70}
-
 	fmt.Println(nilai)
+
+	var values = [...]int{100, 90, 80}
+	fmt.Println(values)
+	fmt.Println(len(values))
+	values[0] = 120 //mengubah nilai pada indeks 0 menjadi 120
+	fmt.Println(values)
 }
